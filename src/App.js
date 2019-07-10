e71b5ade7d90d95a20a93d1e412825ccd7e54424
White <daniel.white@langleywan.com>
2019-07-09 20:54:17 -0400

    added shop page, collection-preview, and collection-item components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
-import HomePage from './pages/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => {
   return (
@@ -16,6 +17,7 @@ function App() {
   return (
     <Switch>
       <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop' component={ShopPage} />
       <Route exact path='/shop/hats' component={HatsPage} />
       <Route exact path='/shop/jackets' component={HatsPage} />
       <Route exact path='/shop/shoes' component={HatsPage} />
